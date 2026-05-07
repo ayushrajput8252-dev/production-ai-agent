a06d51829b395696a0225b6339f12b2414666230
ayushrajput8252t <ayushrajput8252@gmail.com>
2026-05-07 17:27:25 +0530

    added retriver

--- ingest.py
+++ ingest.py
@@ -1,13 +1,17 @@
 from dotenv import load_dotenv
 import os
+import pickle
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
 load_dotenv()
 
 PDF_FOLDER = "C:/AYUSH/openeyes/production-ai-agent/data"
+STORAGE_FOLDER = "storage"
+os.makedirs(STORAGE_FOLDER, exist_ok=True)
+
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -20,3 +24,54 @@ for file in os.listdir(PDF_FOLDER):
         loader = PyPDFLoader(
             os.path.join(PDF_FOLDER, file)
         )
+        docs = loader.load()
+
+        for d in docs:
+            d.metadata["source"] = file
+        
+        document.extend(docs)
+# SPLIT DOCUMENTS
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200,
+    separators = ["\n\n", "\n", ".", " "]
+)
+
+chunks = splitter.split_documents(document)
+
+print(f"Created {len(chunks)} chunks")
+
+with open("storage/chunks.pkl", "wb") as f:
+    pickle.dump(chunks, f)
+
+# EMBEDDINGS
+embeddings = OllamaEmbeddings(
+    model = "mxbai-embed-large"
+)
+
+#pinecone
+pc = Pinecone(api_key = os.getenv("PINECONE_API_KEY"))
+
+existing_indexes = [
+    index["name"]
+    for index in pc.list_indexes()
+]
+if INDEX_NAME not in existing_indexes:
+
+    pc.create_index(
+        name=INDEX_NAME,
+        dimension=1024,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        )
+    )
+print("Pinecone index ready")
+
+PineconeVectorStore.from_documents(
+    documents=chunks,
+    embedding=embeddings,
+    index_name=INDEX_NAME
+)
+print("Documents uploaded to Pinecone")
---
+++ retriever.py
@@ -0,0 +1,56 @@
+import os
+import pickle
+from dotenv import load_dotenv
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
+from sentence_transformers import CrossEncoder
+from pinecone import Pinecone
+
+load_dotenv()
+with open("storage/chunks.pkl", "rb") as f:
+    chunks = pickle.load(f)
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+INDEX_NAME = os.getenv("PINECONE_INDEX")
+
+embeddings = OllamaEmbeddings(
+    model = "mxbai-embed-large"
+)
+# PINECONE VECTOR STORE
+vectorstore = PineconeVectorStore(
+    index_name = INDEX_NAME,
+    embedding = embeddings
+)
+# VECTOR RETRIEVER
+vector_retriever = vectorstore.as_retriever(
+    search_kwargs={"k": 8}
+)
+
+#BM25 
+bm25_retriever = BM25Retriever.from_documents(
+    chunks
+)
+bm25_retriever.k = 8
+
+#hybrid retriiver 
+hybrid_retriever = EnsembleRetriever(
+    retrievers = [vector_retriever, bm25_retriever],
+    weights = [0.5, 0.5]
+)
+
+#reranker-----------------
+reranker = CrossEncoder(
+    model_name = "BAAI/bge-reranker-base"
+)
+
+def retrieve_docs(query):
+    docs = hybrid_retriever.invoke(query)
+
+    pairs = []
+    for doc in docs:
+        pairs.append(
+            (query, doc.page_content)
+        )
